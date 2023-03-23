5b60c341e35a2e967a12bd4f5581723a3487bed5
Kezir <kezir2@gmail.com>
2023-03-23 14:16:14 +0100

    Type_Button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
+public enum QuizType
+{
+    Button = 0,
+    Timer = 1
+}
 public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private TextMeshProUGUI questionText;
     [SerializeField] private QuestionSO[] question;
     [SerializeField] private AnswerButton[] answerButtons = new AnswerButton[4];
     private int currentQuestion;
+    [SerializeField] private QuizType quizType;
+    [SerializeField] private Button nextQuestion;
+    private bool questionAnswered;
 
     void Start()
     {
         currentQuestion = 0;
+        if (quizType == QuizType.Button)
+        {
+            nextQuestion.onClick.AddListener(GoToNextQuestion);
+        }
+        else if(quizType == QuizType.Timer)
+        {
+            nextQuestion.gameObject.SetActive(false);
+        }
         SetUpNewQuestion();
     }
 
     private void SetUpNewQuestion()
     {
+        questionAnswered = false;
         questionText.text = question[currentQuestion].GetQuestion();
 
         int correctIndex = question[currentQuestion].GetCorrectAnswerIndex();
@@ -30,9 +48,21 @@ public class GameManager : Singleton<GameManager>
 
     public void PrepareNewQuestion()
     {
-        currentQuestion++;
+        questionAnswered = true;
         SetUpButtonsState(false);
-        SetUpNewQuestion();
+    }
+
+    private void GoToNextQuestion()
+    {
+        currentQuestion++;
+        if (questionAnswered)
+        {
+            SetUpNewQuestion();
+        }
+        else
+        {
+            SetUpNewQuestion();
+        }
     }
     
     public void ShowCorrectAnswer()
